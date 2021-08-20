34a9458032c9be6b152c4e842cc80e63e67f1d5c
Daniel Apolinário <daniel.cg.rio@hotmail.com>
2021-08-20 00:26:01 -0300

    Projeto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  value = '0';
+  oldValue = '0';
+
+  lastOperator = 'x';
+  readyForNewInput = true;
+  numberGroups = [
+    [7, 8, 9, 'x'], 
+    [4, 5, 6, '-'], 
+    [1, 2, 3, '+'],
+    [0, 'c', '/', '=']
+  ];
+
   constructor() {}
 
+  onButtonPress(symbol) {
+    console.log(symbol);
+
+    
+    if (typeof(symbol) === 'number') {
+      console.log('is a number');
+      if (this.readyForNewInput)
+        this.value = '' + symbol;
+      else
+        this.value += '' + symbol;
+      this.readyForNewInput = false;
+    }
+    else if (symbol === 'c') {
+      this.value = '0';
+      this.readyForNewInput = true;
+    }
+    else if (symbol === '=') {
+      if (this.lastOperator === 'x')
+        this.value = '' + (parseInt(this.oldValue) * parseInt(this.value));
+      else if (this.lastOperator === '-')
+        this.value = '' + (parseInt(this.oldValue) - parseInt(this.value));
+      else if (this.lastOperator === '+')
+        this.value = '' + (parseInt(this.oldValue) + parseInt(this.value));
+      else if (this.lastOperator === '/')
+        this.value = '' + (parseInt(this.oldValue) / parseInt(this.value));
+      this.readyForNewInput = true;
+    }
+    else { // operator
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
+  }
 }
